23e5ad10a7294be64947a0d73236adc8a6b77857
ek-ry <92623105+ek-ry@users.noreply.github.com>
2024-04-02 14:27:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
     $('.featured-card').click(function(event) {
         event.stopPropagation(); 
         
+        // Close any open card
         $('.card-content').not($(this).find('.card-content')).slideUp();
         
+        // Toggle the clicked card
         $(this).find('.card-content').slideToggle();
     });
 
+    // Close the card when clicking outside of it
     $(document).click(function(event) {
         if (!$(event.target).closest('.featured-card').length) {
             $('.card-content').slideUp();
@@ -18,5 +21,3 @@ function DarkMode() {
     var element = document.body;
     element.classList.toggle("darkmode");
 }
-
-
